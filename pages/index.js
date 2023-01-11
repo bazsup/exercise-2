dfe0c10c22b6c24a222bfcb73da69291897599c0
bazsup <bas@odds.team>
2023-01-11 14:03:20 +0700

    change text

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello React</h1>;
+  return <h1>Hello React 2</h1>;
 }
